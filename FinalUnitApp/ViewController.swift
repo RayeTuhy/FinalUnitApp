04a791d59b52fa99333cc2a9ffcc223e80f4e7da
RAY ANNE TUHY <rl200485@rlas-macair-19.local>
2018-12-17 12:05:18 -0600

    did some stuff

--- FinalUnitApp/ViewController.swift
+++ FinalUnitApp/ViewController.swift
@@ -23,7 +23,7 @@ class ViewController: UIViewController {
     }
     
     func parse(number: Int){
-        let jsonUrlString = "https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today/\(number)/"
+        let jsonUrlString = "https://api.sunrise-sunset.org/json?lat=42.3642&lng=42.3642&date=today/\(number)/"
         guard let url = URL(string: jsonUrlString) else { return }
         URLSession.shared.dataTask(with: url) { (data, response, err) in
             guard let data = data else { return }
@@ -34,18 +34,25 @@ class ViewController: UIViewController {
                 let solar_noon = times.solar_noon
                 let day_length = times.day_length
                 print(sunrise, sunset, solar_noon, day_length)
-                self.Times(timesSunrise: sunrise, timesSunset: sunset, timesSolarNoon: solar_noon, timesDayLength: day_length)
+                self.setSolarProperties(timesSunrise: sunrise, timesSunset: sunset, timesSolarNoon: solar_noon, timesDayLength: day_length)
+//                self.times(timesSunrise: sunrise, timesSunset: sunset, timesSolarNoon: solar_noon, timesDayLength: day_length)
             } catch let jsonErr {
                 print("Error Serializing Data:", jsonErr)
             }
-            }.resume()
+        }.resume()
     }
     
-    func characterName(characterName: String, characterHeight: String, characterWeight: String, characterGender: String, characterHairColor: String, characterEyeColor: String) {
+    func setSolarProperties(timesSunrise: String, timesSunset: String, timesSolarNoon: String, timesDayLength: String) {
         DispatchQueue.main.async {
-            self.sunriseLabel.text = "Sunrise: \(timesSunrise)\nSunset: \(timesSunset)\nSolarNoon: \(timesSolarNoon)\nDayLength: \(timesDayLength)"
+            self .sunriseLabel.text = "Sunrise: \(timesSunrise)\nSunset: \(timesSunset)\nSolarNoon: \(timesSolarNoon)\nDayLength: \(timesDayLength)"
         }
     }
+    
+//    func characterName(characterName: String, characterHeight: String, characterWeight: String, characterGender: String, characterHairColor: String, characterEyeColor: String) {
+//        DispatchQueue.main.async {
+//            self.sunriseLabel.text = "Sunrise: \(timesSunrise)\nSunset: \(timesSunset)\nSolarNoon: \(timesSolarNoon)\nDayLength: \(timesDayLength)"
+//        }
+//    }
     
 }
 
